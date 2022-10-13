e6d2691a0452548c964c7e0980163f0ce388241f
yongsin0129 <yongsin0129@gmail.com>
2022-10-13 15:41:17 +0800

    feat : add User Model

---
+++ script.ts
@@ -0,0 +1,28 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main () {
+  /********************************************************************************
+  *
+            create
+  *
+  *********************************************************************************/
+  // const user = await prisma.user.create({
+  //   data: {
+  //     name: 'Alice'
+  //   }
+  // })
+  const users = await prisma.user.findMany()
+  console.log(users)
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async e => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
